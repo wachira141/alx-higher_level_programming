0007f2f9b99dc1c1eea6d2964a16888361a5bfc4
wachira141 <wachiraw75@gmail.com>
2022-09-20 07:50:28 -0700

    fixed few bugs

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i == 99:
-        print(f"{i:02}")
-    else:
-        print(f"{i:02}", end=", ")
+    print("{:0>2}".format(i), end=", " if i < 99 else "\n")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
             i = chr(ord(i) - ord('a') + ord('A'))
-        print(f"{i}", end="")
+        print("{i}".format(i), end="")
     print("")
