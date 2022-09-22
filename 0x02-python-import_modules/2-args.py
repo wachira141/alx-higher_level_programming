752774c141efac5a07150c7ef5943a39267d254c
wachira141 <wachiraw75@gmail.com>
2022-09-22 04:05:28 -0700

    fixed elif functionality

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif length == 2:
         print("{} argument:".format(1))
-        print("{}: {}".format(1, args[1]))
     else:
          print("{} arguments:".format(length -1))
-         while i < length:
-             print("{}: {}".format(i, args[i]))
-             i += 1
+    while i < length:
+        print("{}: {}".format(i, args[i]))
+        i += 1
