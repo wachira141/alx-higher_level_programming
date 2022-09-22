35742503e334772fff665f0b3603c03156aef769
wachira141 <wachiraw75@gmail.com>
2022-09-22 03:37:29 -0700

    fixed 1 agument passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,11 @@ if __name__ == "__main__":
     args = sys.argv
     length = len(args)
     i = 1
-    if length == 1:
+    if length <= 1:
         print("0 arguments.")
+    elif length == 2:
+        print("{} argument:".format(1))
+        print("{}: {}".format(1, args[1]))
     else:
          print("{} arguments:".format(length -1))
          while i < length:
