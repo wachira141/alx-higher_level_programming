8c2b08ad0c2c6253cc6c619a8ebc8899e77c6445
wachira141 <wachiraw75@gmail.com>
2022-09-28 09:28:23 -0700

    sqr matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared = []
-    for line in matrix:
-        squared.append([c**2 for c in line])
-    return squared
+    if matrix is not None:
+        sqr = []
+        for row in matrix:
+            newl = []
+            for col in row:
+                newl.append(col ** 2)
+            sqr.append(newl)
+        return sqr
