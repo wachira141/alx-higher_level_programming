2365e961712dba2ddc6d236c75b06055e74fb596
wachira141 <wachiraw75@gmail.com>
2022-10-25 09:18:55 -0700

    fixed bug

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-Script reads stdin line by line and computes metrics
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-Each 10 lines and after a keyboard interruption (CTRL + C),
-prints those statistics since the beginning:
-total file size and
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-format: File size: <total size>
-format: <status code (in ascending order)>: <number>
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
 
 
