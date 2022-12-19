55c89723fb3f696e12bc2ccf167e48ec6db8ed21
wachira141 <wachiraw75@gmail.com>
2022-12-19 08:31:43 -0800

    fixed file 6 bug

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+
+const texts = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const words of texts) {
+  console.log(words);
 }
