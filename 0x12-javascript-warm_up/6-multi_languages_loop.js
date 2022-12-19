4a5955bc3e9f3a9bff85790756f2fc6e25f4989b
wachira141 <wachiraw75@gmail.com>
2022-12-19 07:58:04 -0800

    multi language

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-while (i < list.length)
-{
-	console.log(list[i]);
-	i++;
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
