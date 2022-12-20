9f3b9a7a55193c3b8146e2c703239df006aa7bad
wachira141 <wachiraw75@gmail.com>
2022-12-20 05:36:00 -0800

    fixed semistandard styles

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-	constructor (w, h) {
-		if (w >0 && h > 0) {
-			this.weight = w;
-			this.height = h;
-		}
-	}
-	print() {
-		for (let column = 0; column < this.height; column += 1) {
-			console.log('X'.repeat(this.width));
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.weight = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let column = 0; column < this.height; column += 1) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 };
