a3a54f8e37aa4f046f62d5143df91ff4344f5461
wachira141 <wachiraw75@gmail.com>
2022-12-20 05:53:10 -0800

    sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,7 @@
-
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-const newDict = {};
-for (const key in dict) {
+let newDict = {};
+for (let key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
   } else {
