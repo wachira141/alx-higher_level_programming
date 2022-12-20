44ac7a837a20734db189861c9eb4d32d16dddf92
wachira141 <wachiraw75@gmail.com>
2022-12-20 05:49:07 -0800

    rectified errors check

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-module.exports = class Rectangle
-{
-	width;
-	height;
-	constructor (w, h){
-		this.width = w;
-		this.height = h;
-	}
-}
-
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
