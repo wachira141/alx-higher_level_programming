bb7f067b3865baf22afbcb44a54267487b7b711e
wachira141 <wachiraw75@gmail.com>
2022-12-22 06:47:20 -0800

    refactor

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,7 +18,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
+    for state in session.query(State).filter(State.name.like('%a%')).all():
+        session.delete(state)
     session.commit()
