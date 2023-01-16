e71f0a692fbe14fe662aa9a97e9fc081d02e6447
wachira141 <wachiraw75@gmail.com>
2023-01-16 04:38:31 -0800

    fixed file 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -s -X PUT -L -d "user_id=98" -H "You got me!" 0.0.0.0:5000/catch_me
+# makes a req to 0.0.0.0:5000/catch_me for a response "You got me!"
+curl -sX PUT -L -d "user_id=98" --header "origin: You got me!" 0.0.0.0:5000/catch_me
