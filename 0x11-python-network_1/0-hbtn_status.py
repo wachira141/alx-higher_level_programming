93bec130bf2436d9ab6012ababd695dab3c7212f
wachira141 <wachiraw75@gmail.com>
2023-01-17 08:08:20 -0800

    task one

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         data = response.read()
         print("Body response:")
-        print("\t - type: {}".format(type(data)))
-        print("\t - content: {}".format(data))
-        print("\t - utf8 content: {}".format(data.decode('utf-8')))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8')))
